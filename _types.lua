ac26feb27642a6a7e411c936568ff0350a03c690
Frowmza <66181451+Frowmza@users.noreply.github.com>
2024-02-13 00:19:15 +0100

    chore(types)

--- _types.lua
+++ _types.lua
@@ -3,12 +3,12 @@
 ---@class SpriteParamBase
 ---@field public id? integer @ The handle id of the sprite. Assigned on creation
 ---@field public spriteType? 'default' | 'entity' | 'bone' @ The type of the sprites. Assigned on creation
----@field public key string @ The displayed in the middle of the sprite
----@field public colour number[] @ The colour of the sprite. RGB array
+---@field public key? string @ The displayed in the middle of the sprite
+---@field public colour? number[] @ The colour of the sprite. RGB array
 ---@field public keyColour? number[] @ The colour of the key. RGB array
----@field public shape string  @ The shape of the sprite
----@field public scale number @ Multiplier for the sprite size
----@field public distance number @ The distance from the sprite to the player
+---@field public shape? string  @ The shape of the sprite
+---@field public scale? number @ Multiplier for the sprite size
+---@field public distance? number @ The distance from the sprite to the player
 ---@field public onEnter? function @ The function to call when the player enters the sprite
 ---@field public onExit? function @ The function to call when the player exits the sprite
 ---@field public nearby? function @ The function to call when the player is nearby the sprite
@@ -18,7 +18,6 @@
 
 ---@class DefinedSpriteParam : SpriteParamBase
 ---@field public coords vector3 @ The coordinates of the sprite
----@field public distance number @ The distance from the sprite to the player
 
 ---@class EntitySpriteParam : SpriteParamBase
 ---@field public entity number @ The entity to track the bone of
